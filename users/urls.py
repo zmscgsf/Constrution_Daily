8d70f904a9464544401dea5b909be280e914420f
zmscgsf <2010909388@qq.com>
2020-02-08 01:36:14 +0800

    增加用户注销链接

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ app_name = 'user'
 urlpatterns = [
     # path('login/', login, {'template_name', 'user/login.html'}, name='admin'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+from django.contrib.auth import logout
+
+
+def logout_view(request):
+    """注销用户"""
+    logout(request)
+    return HttpResponseRedirect(reverse('note:index'))
